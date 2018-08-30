67205caab72d27ca9ed87835d4834f5ec94c0168
Michael Haß <michael.s.hass@googlemail.com>
2018-08-30 10:32:45 +0200

    Set xcode build to workspace

--- .travis/build.sh
+++ .travis/build.sh
@@ -8,15 +8,15 @@ xcodebuild -showsdks;
 
 # Build Framework in Debug and Run Tests if specified
 if [ $RUN_TESTS == "YES" ]; then
-    xcodebuild -workspace "CellularLocalStorage.xcodeproj" -scheme "$SCHEME" -destination "$DESTINATION" -configuration Debug ONLY_ACTIVE_ARCH=NO ENABLE_TESTABILITY=YES test | xcpretty;
+    xcodebuild -workspace "CellularLocalStorage.xcworkspace" -scheme "$SCHEME" -destination "$DESTINATION" -configuration Debug ONLY_ACTIVE_ARCH=NO ENABLE_TESTABILITY=YES test | xcpretty;
 else
-    xcodebuild -workspace "CellularLocalStorage.xcodeproj" -scheme "$SCHEME" -destination "$DESTINATION" -configuration Debug ONLY_ACTIVE_ARCH=NO build | xcpretty;
+    xcodebuild -workspace "CellularLocalStorage.xcworkspace" -scheme "$SCHEME" -destination "$DESTINATION" -configuration Debug ONLY_ACTIVE_ARCH=NO build | xcpretty;
 fi
 # Build Framework in Release and Run Tests if specified
 if [ $RUN_TESTS == "YES" ]; then
-    xcodebuild -workspace "CellularLocalStorage.xcodeproj" -scheme "$SCHEME" -destination "$DESTINATION" -configuration Release ONLY_ACTIVE_ARCH=NO ENABLE_TESTABILITY=YES test | xcpretty;
+    xcodebuild -workspace "CellularLocalStorage.xcworkspace" -scheme "$SCHEME" -destination "$DESTINATION" -configuration Release ONLY_ACTIVE_ARCH=NO ENABLE_TESTABILITY=YES test | xcpretty;
 else
-    xcodebuild -workspace "CellularLocalStorage.xcodeproj" -scheme "$SCHEME" -destination "$DESTINATION" -configuration Release ONLY_ACTIVE_ARCH=NO build | xcpretty;
+    xcodebuild -workspace "CellularLocalStorage.xcworkspace" -scheme "$SCHEME" -destination "$DESTINATION" -configuration Release ONLY_ACTIVE_ARCH=NO build | xcpretty;
 fi
 # Run `pod lib lint` if specified
 if [ $POD_LINT == "YES" ]; then
