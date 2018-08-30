d2c07f7c31503b083b8820d06096ed5b847f6317
Michael Haß <michael.s.hass@googlemail.com>
2018-08-30 10:25:31 +0200

    Update travis files

---
+++ .travis/build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e; # Fail on first error
+
+# Prepare
+set -o pipefail; # xcpretty
+xcodebuild -version;
+xcodebuild -showsdks;
+
+# Build Framework in Debug and Run Tests if specified
+if [ $RUN_TESTS == "YES" ]; then
+    xcodebuild -workspace "CELLULAR.xcodeproj" -scheme "$SCHEME" -destination "$DESTINATION" -configuration Debug ONLY_ACTIVE_ARCH=NO ENABLE_TESTABILITY=YES test | xcpretty;
+else
+    xcodebuild -workspace "CELLULAR.xcodeproj" -scheme "$SCHEME" -destination "$DESTINATION" -configuration Debug ONLY_ACTIVE_ARCH=NO build | xcpretty;
+fi
+# Build Framework in Release and Run Tests if specified
+if [ $RUN_TESTS == "YES" ]; then
+    xcodebuild -workspace "CELLULAR.xcodeproj" -scheme "$SCHEME" -destination "$DESTINATION" -configuration Release ONLY_ACTIVE_ARCH=NO ENABLE_TESTABILITY=YES test | xcpretty;
+else
+    xcodebuild -workspace "CELLULAR.xcodeproj" -scheme "$SCHEME" -destination "$DESTINATION" -configuration Release ONLY_ACTIVE_ARCH=NO build | xcpretty;
+fi
+# Run `pod lib lint` if specified
+if [ $POD_LINT == "YES" ]; then
+    pod lib lint;
+fi
