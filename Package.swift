e2021714ebefe6ea89b86f758b8d13b3a7dbcd59
BenediktHr <BenediktHr@icloud.com>
2020-03-30 14:10:16 +0200

    Use 'feature/xcode_11.4' branch for 'cellular-swift'

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         .library(name: "LocalStorage", type: .dynamic, targets: ["LocalStorage"])
     ],
     dependencies: [
-        .package(url: "https://github.com/cellular/cellular-swift.git", .branch("master"))
+        .package(url: "https://github.com/cellular/cellular-swift.git", .branch("feature/xcode_11.4"))
     ],
     targets: [
         .target(name: "LocalStorage", dependencies: ["CELLULAR"]),
